441a69532f88d5bc03f6cb26e8be4cc1a0f553b8
Wen-Tien Chang <ihower@gmail.com>
2015-03-24 16:16:18 +0800

    Roman 2

--- roman/kata.js
+++ roman/kata.js
@@ -1,5 +1,27 @@
 exports.convert = function(n){
 
-  return Array(n+1).join("I");
+  var str = "";
+
+  while ( n >= 10 ) {
+    str += "X";
+    n -= 10
+  }
+
+  while ( n >= 9 ) {
+    str += "IX";
+    n -= 9
+  }
+
+  while ( n >= 5 ) {
+    str += "V";
+    n -= 5
+  }
+
+  while ( n >= 4 ) {
+    str += "IV";
+    n -= 4
+  }
+
+  return str + Array(n+1).join("I");
 
 }
--- roman/spec/kata_spec.js
+++ roman/spec/kata_spec.js
@@ -10,4 +10,29 @@ describe("roman", function () {
     expect( kata.convert(2) ).toBe("II");
   });
 
+  it("should convert 4 to IV", function () {
+    expect( kata.convert(4) ).toBe("IV");
+  });
+
+
+  it("should convert 5 to V", function () {
+    expect( kata.convert(5) ).toBe("V");
+  });
+
+  it("should convert 9 to IX", function () {
+    expect( kata.convert(9) ).toBe("IX");
+  });
+
+  it("should convert 10 to X", function () {
+    expect( kata.convert(10) ).toBe("X");
+  });
+
+  it("should convert 20 to XX", function () {
+    expect( kata.convert(20) ).toBe("XX");
+  });
+
+  it("should convert 21 to XX", function () {
+    expect( kata.convert(21) ).toBe("XXI");
+  });
+
 });
