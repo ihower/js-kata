ecf58063e17c8e25448582a0ff05c5e954eb6a91
Wen-Tien Chang <ihower@gmail.com>
2015-03-24 16:19:32 +0800

    Roman 4

--- roman/kata.js
+++ roman/kata.js
@@ -1,31 +1,20 @@
 exports.convert = function(n){
 
   var str = "";
+  var table = [
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"]
+  ]
 
-  while ( n >= 10 ) {
-    str += "X";
-    n -= 10
-  }
+  table.forEach(function(t) {
+    while ( n >= t[0] ) {
+      str += t[1];
+      n -= t[0];
+    }
+  })
 
-  while ( n >= 9 ) {
-    str += "IX";
-    n -= 9
-  }
-
-  while ( n >= 5 ) {
-    str += "V";
-    n -= 5
-  }
-
-  while ( n >= 4 ) {
-    str += "IV";
-    n -= 4
-  }
-
-  while (n >= 1 ) {
-    str += "I";
-    n -= 1
-  }
   return str;
-
 }
