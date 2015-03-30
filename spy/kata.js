da788d5b92b91ba9167827dc1e3337460d188578
Wen-Tien Chang <ihower@gmail.com>
2015-03-30 18:34:28 +0800

    Spy example

---
+++ spy/kata.js
@@ -0,0 +1,5 @@
+exports.generate_awesome = function(n){
+
+  return "awesome"
+
+}
---
+++ spy/spec/kata_spec.js
@@ -0,0 +1,19 @@
+var kata = require("../kata")
+
+describe("generate_awesome", function () {
+
+  it("should generate awesome string", function () {
+    var foo = {
+      bar: function() { console.log("QQ") }
+    };
+
+    spyOn(foo, 'bar').and.returnValue(456);
+
+    foo.bar(123);
+
+    expect( foo.bar ).toHaveBeenCalled();
+    expect( foo.bar(123) ).toBe(456); //toHaveBeenCalledWith(123);
+
+  });
+
+});
