16ba8fb94578387e9ace6a32bcee2615fca53745
Wen-Tien Chang <ihower@gmail.com>
2015-03-30 17:24:37 +0800

    Order#amount spec

--- order/spec/order_spec.js
+++ order/spec/order_spec.js
@@ -8,11 +8,30 @@ describe("order", function () {
 
     it("should have default status is New", function () {
       expect(order.status).toBe("New");
-    });
+    })
 
     it("should have default amount is 0", function () {
       expect(order.amount).toBe(0);
-    });
+    })
 
-  });
-});
+  })
+
+  describe("#amount", function() {
+    describe("when user is vip",function(){
+      it("should discount five percent if total > 1000", function () {
+        //...
+      })
+
+      it("should discount ten percent if total > 10000", function () {
+        //...
+      })
+    })
+
+   describe("when user is not vip", function() {
+      it("should discount three percent if total > 10000", function () {
+        //...
+      })
+    })
+  })
+
+})
