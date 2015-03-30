96ba1f9fdddf5505a705411e4fc1ef6c012e3109
Wen-Tien Chang <ihower@gmail.com>
2015-03-30 18:39:50 +0800

    Downgrade for jasmine-node

--- spy/spec/kata_spec.js
+++ spy/spec/kata_spec.js
@@ -7,7 +7,8 @@ describe("generate_awesome", function () {
       bar: function() { console.log("QQ") }
     };
 
-    spyOn(foo, 'bar').and.returnValue(456);
+    spyOn(foo, 'bar').andReturn(456);
+    //spyOn(foo, 'bar').and.returnValue(456);
 
     foo.bar(123);
 
