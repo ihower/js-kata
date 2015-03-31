fc4dfaa8442e3991f464af03e152280f73e89316
Wen-Tien Chang <ihower@gmail.com>
2015-03-31 19:04:10 +0800

    Refactor leap year

--- leap_year/kata.js
+++ leap_year/kata.js
@@ -1,17 +1,5 @@
 exports.is_leap_year = function(y){
 
-  if ( y % 4 == 0 ) {
-    if ( y % 100 == 0 ) {
-      if ( y % 400 == 0 ) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      return true;
-    }
-  } else {
-    return false;
-  }
+  return y % 400 == 0 || ( y % 4 == 0 && y % 100 !=0 )
 
 }
